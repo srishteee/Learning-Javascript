a0305d69061254b0df2c3771a21c7415a29dd06f
srishteee <srishtinagarkoti.25@gmail.com>
2024-09-21 22:56:31 +0530

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,30 @@
 "use strict "  // it bascially treats all JS code as a newer versions
 
-alert(3+3)  // we are using noe js not browser
+// alert(3+3)  // we are using noe js not browser
 
 console.log(3
-    3
-);  // code readiablity should be higher priority
+    +3
+
+);  // code readiablity should be higher priority\
+
+
+
+let name = "Srihti"
+let age = 24
+let isLoggedIn = false
+let state = null
+// Primitive data types
+// number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => it is standalone value in JS , it i specail type which is empty which is not undefined 
+// undefined => value which is not  defined 
+// symbol -- to find the uniqueness used symbol
+
+console.log(typeof name);  // tyepeof is used to find the type of the primitve data types or to find the type 
+console.log(typeof null);   // null is an object type  
+
+console.log(typeof undefined);  // undefined is undefined type only
+
+
