fd45d3277334f781c77c3c505adc941d5d0c48f2
srishteee <srishtinagarkoti.25@gmail.com>
2024-09-21 22:19:08 +0530

    Learning React from Scratch

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553 
+let accountEmail = 'nagar@gmail.com'
+var accountPassword = "12345"
+accountCity = 'delhi'
+
+
+accountEmail = "xyz.google.com";
+accountPassword='234245'
+accountCity='jaipur'
+console.log(accountID);
+console.table([accountEmail,accountPassword,accountCity]);   // easiest way to make in a tabular form
+
+
+// prefer to use let instead of var becaus eof block and functional scope
