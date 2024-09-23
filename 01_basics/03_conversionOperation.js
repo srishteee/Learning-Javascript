35114aaa944ca76ac308d526df7236a2e5c2588b
srishteee <srishtinagarkoti.25@gmail.com>
2024-09-23 16:13:02 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "44"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" +> 33 easily convert
+//"33abc"=> NaN = not able to convert in numbers
+// True => 1
+// false => 0
+
+let booleanILoggedIn = Boolean (isLoggedIn)
+console.log(booleanILoggedIn);
+//while converting
+// "rhea" => false
+// 1 ==> true
+// 0 ==> false
+
+// to convert in strings
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber)
+
+console.log(typeof stringNumber);
+
+
