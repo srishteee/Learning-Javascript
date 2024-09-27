aed131698c67b3f7975364ba715e2048da2ab355
srishteee <srishtinagarkoti.25@gmail.com>
2024-09-27 15:46:30 +0530

    Learning JS from Scratch

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,21 @@
-let score = "44"
+// let score = "44"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "33" +> 33 easily convert
 //"33abc"=> NaN = not able to convert in numbers
 // True => 1
 // false => 0
 
-let booleanILoggedIn = Boolean (isLoggedIn)
-console.log(booleanILoggedIn);
+// let booleanILoggedIn = Boolean (isLoggedIn)
+// console.log(booleanILoggedIn);
 //while converting
 // "rhea" => false
 // 1 ==> true
@@ -23,10 +23,44 @@ console.log(booleanILoggedIn);
 
 // to convert in strings
 
-let someNumber = 33
-let stringNumber = String (someNumber)
-console.log(stringNumber)
+// let someNumber = 33
+// let stringNumber = String (someNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************  Operations *****************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2/2)
+
+// let str1 = "hello"
+// let str2 = " Rhea"
+
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 1 );
+
+// console.log(1 + 3 + "1")
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+num1=num2=num3= 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
 
 
