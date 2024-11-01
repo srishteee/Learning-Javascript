0fd175a5c5a6f54549f8003bd9da0b6851d12844
srishteee <srishtinagarkoti.25@gmail.com>
2024-11-02 00:50:07 +0530

    Learning JS from Scratch

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,48 @@ if(true){
     const b=20;
     //var c = 30;  // if you not declare still getting the same problem
 }
- console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);   // it is accesible outside the block that is why not good to use var
 
 // whenever we check the global scope in vscde via node this scoe is different from the browser(global) scope 
+
+//++++++   function inside functionsss
+
+function one (){
+    const username= "rhea"
+
+    function two (){   // two fn can access the one 
+        const website = "youtube"
+       // console.log(username)
+        
+    }
+
+    //console.log(website)
+   // two()
+}
+//one()
+
+
+if (true) {
+  const username ="rheaaa"
+  if(username==="rheaaa"){
+    const website = "  youtube"
+  
+    //console.log(username+website)
+} 
+//console.log(website) 
+}
+//console.log(username)
+
+
+/// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+addone(5)
+function addone (num){
+    return num+1;
+}
+
+addTwo(5)  // it gives error bec it store in variable hold in variable and this cncept is hoisting 
+const addTwo = function(num){
+    return num +2;
+}
