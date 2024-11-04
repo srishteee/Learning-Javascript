ab3cb4dd3893250b9f45bb9466a80511382cd575
srishteee <srishtinagarkoti.25@gmail.com>
2024-11-04 09:20:02 +0530

    Learning JS from Scratch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);  // this cannot use bec it is out of the block but when we use var iut can be accessible
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");   // this is short hand  we cannot use {} it is called implicit scope we can use , for use another log but it is unreadable
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {   // || this is OR 
+    console.log("User logged in");
+}   
