b2df1de968a409179055b29dd819e66ff6cb6d23
Srishteee <srishtinagarkoti.25@gmail.com>
2024-11-07 10:35:37 +0530

    Learning JS from Scratch

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+
+
+// let index =0;
+// while (index<=10) {
+//     console.log(`index value of ${index}`)
+//     index = index+2;
+    
+// }
+
+// let myArray =[`spiderman, batman, hulk`]
+// let arr=0;
+// console.log(myArray.length)
+// while (arr<myArray.length) {
+
+//     console.log(`value of my arr ${myArray[arr]}`)
+//     arr=arr+1;
+    
+// }
+
+let score =11
+do {
+    console.log(`score is ${score}`);
+score++
+    
+} while (score<=10);
